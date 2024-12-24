4783e8978250fff56f7a42663667faa0a97432b4
unknown <zz240614@gmail.com>
2024-12-24 16:01:26 +0100

    CREAT DB Contacts

---
+++ contact.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Contactify ; 
+USE Contactify ; 
+
+CREATE TABLE Contacts(
+    id INT PRIMARY KEY AUTO_INCREMENT ,
+    photo VARCHAR (250), 
+    nom VARCHAR (5O), 
+    prenom VARCHAR (5O), 
+    email VARCHAR (5O), 
+    numero  VARCHAR (5O) 
+);
+
+INSERT INTO contacts (photo, nom, prenom, email, numero)
+VALUES
+('https://cdn.sofifa.net/flags/ar.png', 'kardache' ,'zakaria' , 'ardache.@mail.cc','63456756'),
+('https://cdn.sofifa.net/flags/ar.png', 'kardache' , 'ahmed' , 'dache.@mail.cc','63456756');
